20b5fb5f3aafa5dca9ce583333cb7b3d8f73ac20
Mark <mark@highway.localdomain>
2019-10-28 15:26:54 -0500

    Upgrade unifi to 5.11.50

--- unifi/docker-run-unifi.sh
+++ unifi/docker-run-unifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UNIFI_VERSION=5.11.46
+UNIFI_VERSION=5.11.50
 
 docker run -dit --restart unless-stopped --init -p 8080:8080 -p 8443:8443 -p 3478:3478/udp -p 10001:10001/udp -e RUNAS_UID0=false -e UNIFI_UID=1001 -e UNIFI_GID=1001 -e TZ='America/Chicago' -e JVM_MAX_THREAD_STACK_SIZE=1280k -v /opt/UniFi:/unifi --name unifi jacobalberty/unifi:$UNIFI_VERSION
 
