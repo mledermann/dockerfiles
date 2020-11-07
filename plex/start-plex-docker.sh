2ac2eceeb84fd1a505a31d30a72a5701642e9992
mledermann <32495879+mledermann@users.noreply.github.com>
2020-11-07 07:42:28 -0600

    version 1.20.4

--- plex/start-plex-docker.sh
+++ plex/start-plex-docker.sh
@@ -9,7 +9,7 @@ if [[ -z "$PLEX_IP" ]]; then
   echo "Must set PLEX_IP to the ip to connect to"
   exit 1
 fi
-PLEX_VERSION=1.19.3.2843-e3c1f7bcd
+PLEX_VERSION=1.20.4.3517-ab5e1197c
 
 docker run \
 -d \
