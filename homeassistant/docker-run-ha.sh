993e64a16bf4cf620ef12a95acf04eda66f43690
mledermann <32495879+mledermann@users.noreply.github.com>
2021-12-29 16:15:00 -0600

    pull image before start

--- homeassistant/docker-run-ha.sh
+++ homeassistant/docker-run-ha.sh
@@ -2,5 +2,6 @@
 
 HA_VERSION=stable
 
-docker run -dit --restart unless-stopped -p 8123:8123 -e TZ='America/Chicago' --name homeassistant homeassistant/home-assistant:$HA_VERSION
+docker pull homeassistant/home-assistant:$HA_VERSION
+docker run -dit --restart unless-stopped -p 8123:8123 -p 8989:8989 -e TZ='America/Chicago' -v /home/mark/data/homeassistant/config:/config:z --name homeassistant homeassistant/home-assistant:$HA_VERSION
 
